d4f1cfbf0460f662967ebdb824ed9793885e03ac
jebritash-tech <jebritash@gmail.com>
2026-08-28 10:15:07 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/setup-system', function () {
 
+    Artisan::call('migrate:fresh', [
+        '--force' => true
+    ]);
+
+    Artisan::call('db:seed', [
+        '--force' => true
+    ]);
+
+    return response()->json([
+        'success' => true,
+        'message' => 'System initialized successfully'
+    ]);
